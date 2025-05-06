74583121b3a1323644500c8219c0e140bc7b8670
mlkshkck <ollehme@gmail.com>
2025-05-06 15:40:51 +0900

    [FEAT] home is visible

--- vite-project/src/Home.jsx
+++ vite-project/src/Home.jsx
@@ -3,37 +3,68 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="container">
-      <h1 className="main-title">딥러닝 기반 PRN 코드 이상 탐지</h1>
-
-      <p className="input-or-file">입력 or 파일첨부 택1</p>
-      <label className="input-label" htmlFor="input-textarea">
-        &lt;입력값&gt;
-      </label>
-      <textarea id="input-textarea" className="input-textarea" />
-
-      <label className="checkbox-label" htmlFor="checkbox">
-        <input type="checkbox" id="checkbox" className="checkbox" />
-        Checkbox Label
-      </label>
-
-      <p className="file-label">&lt;파일첨부&gt;</p>
-      <div className="file-box">
-        <p className="drag-drop-text">첨부파일을 마우스로 끌어 놓으세요</p>
-      </div>
-
-      <div className="gray-box-1">{/* Content inside gray box */}</div>
-
-      <label className="label-e1" htmlFor="input-box">
-        E1
-      </label>
-      <div className="input-box" id="input-box" />
-
-      <button className="submit-button">
-        <span className="submit-button-text">입력</span>
-      </button>
+    <div className="home-container">
+      <div className="rectangle-1262x188" />
+      <div className="rectangle-128x28" />
+      <div className="text-input">입력</div>
+      <div className="title-main">딥러닝 기반 PRN 코드 이상 탐지</div>
+      <div className="label-input-value">&lt;입력값&gt;</div>
+      <div className="label-file-attachment">&lt;파일첨부&gt;</div>
+      <div className="text-input-or-file">입력 or 파일첨부 택1</div>
+      <div className="checkbox-box" />
+      <div className="vertical-bar" />
+      <img
+        className="image-placeholder-large"
+        src="https://placehold.co/1443x132"
+        alt="placeholder"
+      />
+      <div className="drag-drop-text">첨부파일을 마우스로 끌어 놓으세요</div>
+      <div className="rectangle-1262x649" />
+      <div className="text-d">𝒅</div>
+      <div className="text-delta">𝚫</div>
+      <div className="rectangle-light-1" />
+      <div className="rectangle-light-2" />
+      <div className="text-file-info">일반파일 0KB/ 최대10MB</div>
+      <img
+        className="image-placeholder-small"
+        src="https://placehold.co/314x98"
+        alt="placeholder"
+      />
+      <div className="text-e1">E1</div>
+      <div className="rectangle-light-3" />
+      <div className="text-e2">E2</div>
+      <div className="text-e3">E3</div>
+      <div className="text-e4">E4</div>
+      <div className="text-l1">L1</div>
+      <div className="text-l2">L2</div>
+      <div className="text-l3">L3</div>
+      <div className="text-l4">L4</div>
+      <div className="rectangle-light-4" />
+      <div className="rectangle-light-5" />
+      <div className="rectangle-light-6" />
+      <div className="rectangle-light-7" />
+      <div className="rectangle-light-8" />
+      <div className="rectangle-light-9" />
+      <div className="rectangle-light-10" />
+      <div className="text-distortion">왜곡값 입력 (선택사항)</div>
+      <div className="rectangle-light-11" />
+      <div className="rectangle-light-12" />
+      <div className="checkbox-white-1" />
+      <div className="checkbox-outline-1" />
+      <div className="checkbox-white-2" />
+      <div className="checkbox-outline-2" />
+      <div className="checkbox-white-3" />
+      <div className="checkbox-outline-3" />
+      <div className="text-f">𝒇</div>
+      <div className="text-sigma">𝝈</div>
+      <div className="text-ranging-error">ranging error</div>
+      <div className="link-latest-graph">최신 ranging error 그래프 보기 &gt;</div>
+      <div className="rectangle-dark" />
+      <div className="link-file-data">파일 데이터 확인하기 &gt; </div>
+      <div className="link-all-data">전체 데이터 확인하기 &gt; </div>
+      {/* More elements below with corresponding classNames */}
     </div>
   );
-}
+};
 
 export default Home;
