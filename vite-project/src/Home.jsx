013f8d50ce923acaa97e57040dfdc90bf1c6ee91
mlkshkck <ollehme@gmail.com>
2025-05-06 22:06:55 +0900

    [FEAT] home visualizing complete

--- vite-project/src/Home.jsx
+++ vite-project/src/Home.jsx
@@ -1,68 +1,124 @@
 import React from "react";
 import "./Home.css";
 
+import logo from "./assets/lab_logo.png";
+import contacts from "./assets/contacts.png";
+import upload from "./assets/upload.svg";
+
 const Home = () => {
   return (
-    <div className="home-container">
-      <div className="rectangle-1262x188" />
-      <div className="rectangle-128x28" />
-      <div className="text-input">입력</div>
-      <div className="title-main">딥러닝 기반 PRN 코드 이상 탐지</div>
-      <div className="label-input-value">&lt;입력값&gt;</div>
-      <div className="label-file-attachment">&lt;파일첨부&gt;</div>
-      <div className="text-input-or-file">입력 or 파일첨부 택1</div>
-      <div className="checkbox-box" />
-      <div className="vertical-bar" />
-      <img
-        className="image-placeholder-large"
-        src="https://placehold.co/1443x132"
-        alt="placeholder"
-      />
-      <div className="drag-drop-text">첨부파일을 마우스로 끌어 놓으세요</div>
-      <div className="rectangle-1262x649" />
-      <div className="text-d">𝒅</div>
-      <div className="text-delta">𝚫</div>
-      <div className="rectangle-light-1" />
-      <div className="rectangle-light-2" />
-      <div className="text-file-info">일반파일 0KB/ 최대10MB</div>
-      <img
-        className="image-placeholder-small"
-        src="https://placehold.co/314x98"
-        alt="placeholder"
-      />
-      <div className="text-e1">E1</div>
-      <div className="rectangle-light-3" />
-      <div className="text-e2">E2</div>
-      <div className="text-e3">E3</div>
-      <div className="text-e4">E4</div>
-      <div className="text-l1">L1</div>
-      <div className="text-l2">L2</div>
-      <div className="text-l3">L3</div>
-      <div className="text-l4">L4</div>
-      <div className="rectangle-light-4" />
-      <div className="rectangle-light-5" />
-      <div className="rectangle-light-6" />
-      <div className="rectangle-light-7" />
-      <div className="rectangle-light-8" />
-      <div className="rectangle-light-9" />
-      <div className="rectangle-light-10" />
-      <div className="text-distortion">왜곡값 입력 (선택사항)</div>
-      <div className="rectangle-light-11" />
-      <div className="rectangle-light-12" />
-      <div className="checkbox-white-1" />
-      <div className="checkbox-outline-1" />
-      <div className="checkbox-white-2" />
-      <div className="checkbox-outline-2" />
-      <div className="checkbox-white-3" />
-      <div className="checkbox-outline-3" />
-      <div className="text-f">𝒇</div>
-      <div className="text-sigma">𝝈</div>
-      <div className="text-ranging-error">ranging error</div>
-      <div className="link-latest-graph">최신 ranging error 그래프 보기 &gt;</div>
-      <div className="rectangle-dark" />
-      <div className="link-file-data">파일 데이터 확인하기 &gt; </div>
-      <div className="link-all-data">전체 데이터 확인하기 &gt; </div>
-      {/* More elements below with corresponding classNames */}
+    <div className="container">
+      <div className="lab-logo-nest">
+        <button
+          style={{
+            marginTop: "25px",
+            marginLeft: "22px",
+          }}
+        >
+          <img className="lab-logo" src={logo} alt="lab logo" />
+        </button>
+      </div>
+      <div className="main-container">
+        <div className="confirm-input-button" />
+        <div className="title-main">딥러닝 기반 PRN 코드 이상 탐지</div>
+        <div className="redirection-nest">
+          <button className="redirection" a="">
+            최신 Ranging Error 그래프 보기 &gt;
+          </button>
+          <button className="redirection" a="">
+            파일 데이터 확인하기 &gt;
+          </button>
+          <button className="redirection" a="">
+            전체 데이터 확인하기 &gt;
+          </button>
+        </div>
+        <div className="text-left-nest">
+          <div className="text-input-or-file">입력 or 파일첨부 택1</div>
+          <div className="text-bracket">&lt;입력값&gt;</div>
+        </div>
+        <div className="input-nest">
+          <div
+            className="input-subnest"
+            style={{ borderRight: "1px #1639B9 solid" }}
+          >
+            <div className="input-instance">
+              E1
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance">
+              E2
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance">
+              E3
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance">
+              E4
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance">
+              L1
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance">
+              L2
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance">
+              L3
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance">
+              L4
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance" id="ranging-error">
+              Ranging Error
+              <div className="stringbox"></div>
+            </div>
+          </div>
+          <div className="input-subnest">
+            <div className="text-left-nest" id="distortion">
+              왜곡값 입력 (선택사항)
+            </div>
+            <div className="input-instance">
+              <div className="checkbox"></div>
+              <div className="input-text-box">𝚫</div>
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance">
+              <div className="checkbox"></div>
+              <div className="input-text-box">
+                𝒇<sub>𝒅</sub>
+              </div>
+              <div className="stringbox"></div>
+            </div>
+            <div className="input-instance">
+              <div className="checkbox"></div>
+              <div className="input-text-box">𝝈</div>
+              <div className="stringbox"></div>
+            </div>
+          </div>
+        </div>
+        <div style={{ height: "47px" }} />
+        <div className="text-left-nest">
+          <div className="text-bracket">&lt;파일첨부&gt;</div>
+        </div>
+        <div className="drag-drop-nest">
+          <div className="file-info-text">일반파일 0KB/ 최대10MB</div>
+          <div className="drag-drop-text">
+            <img className="upload" src={upload} alt="upload" />
+            첨부파일을 마우스로 끌어 놓으세요
+          </div>
+        </div>
+        <div className="input-confirm-nest">
+          <button className="input-confirm">입력</button>
+        </div>
+      </div>
+      <div className="contacts-Nest">
+        <img className="contacts" src={contacts} alt="contacts" />
+      </div>
     </div>
   );
 };
