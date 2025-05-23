d33c492645911b054b6b80b7696d84ba96ca6584
jiheeha <hjh567345@naver.com>
2025-05-24 01:55:23 +0900

    graph.css수정

--- vite-project/src/Pages/Graph.jsx
+++ vite-project/src/Pages/Graph.jsx
@@ -1,50 +1,25 @@
-// import frame13213178492 from "./frame-1321317849-2.svg";
-// import frame1321317849 from "./frame-1321317849.svg";
-// import frame1321317850 from "./frame-1321317850.svg";
-// import image4 from "./image-4.png";
-// import image from "./image.svg";
-// import vector4609 from "./vector-4609.svg";
-// import vector from "./vector.svg";
-
+import React from "react";
 import "../CSS/Graph.css";
 
 const Graph = () => {
   return (
-    <div className="ranging-error">
-      <div className="div">
-        <div className="flexcontainer">
-          <div className="main-container">
-            <div className="confirm-input-button" />
-          </div>
-        </div>
+    <div className="graph-container">
+      <div className="graph-header">
+        <h2 className="graph-title">Ranging Error Graph (정상 데이터)</h2>
+        <button className="graph-save-button">저장하기</button>
+      </div>
 
-        <p className="p">Ranging Error Graph (정상 데이터)</p>
+      <div className="graph-info">
+        <span className="sample-count">샘플 개수: 00000000</span>
+      </div>
 
-        <div className="arrow">
-          {/* <img className="img" alt="Frame" src={frame1321317849} /> */}
-        </div>
-
-        <div className="div-wrapper">
-          <div className="text-wrapper-3">저장하기</div>
-        </div>
-
-        <div className="frame-2">
-          <div className="arrow">
-            {/* <img className="img" alt="Frame" src={image} /> */}
-          </div>
-        </div>
-
-        <div className="frame-3">
-          <div className="arrow">
-            {/* <img className="img" alt="Frame" src={frame13213178492} /> */}
-          </div>
-        </div>
-
-        <div className="text-wrapper-5">샘플 개수: 00000000</div>
-
-        <div className="image-wrapper">
-          {/* <img className="image" alt="Image" src={image4} /> */}
-        </div>
+      <div className="graph-image-wrapper">
+        {/* <img className="img" alt="Frame" src={image} /> */}
+        <img
+          src="/assets/graph-image.png"
+          alt="Ranging Error Graph"
+          className="graph-image"
+        />
       </div>
     </div>
   );
