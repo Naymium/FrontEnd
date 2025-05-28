40ec8a4ccd4bdddf89693bb5b328277ff2d9475d
jiheeha <hjh567345@naver.com>
2025-05-28 12:51:29 +0900

    graph final

--- vite-project/src/Pages/Graph.jsx
+++ vite-project/src/Pages/Graph.jsx
@@ -1,25 +1,20 @@
-import React from "react";
 import "../CSS/Graph.css";
 
 const Graph = () => {
   return (
     <div className="graph-container">
-      <div className="graph-header">
-        <h2 className="graph-title">Ranging Error Graph (정상 데이터)</h2>
-        <button className="graph-save-button">저장하기</button>
+      <div className="graph-section-title">
+        Ranging Error Graph (정상 데이터)
       </div>
 
-      <div className="graph-info">
-        <span className="sample-count">샘플 개수: 00000000</span>
+      <div className="graph-sample-save-wrapper">
+        <div className="sample-count">샘플 개수: 00000000</div>
+        <button className="save-button">저장하기</button>
       </div>
 
-      <div className="graph-image-wrapper">
-        {/* <img className="image" alt="Image" src={image4} /> */}
-        <img
-          src="/assets/graph-image.png"
-          alt="Ranging Error Graph"
-          className="graph-image"
-        />
+      <div className="graph-box">
+        {/* 여기에 그래프 이미지나 컴포넌트 삽입 */}
+        {/* <img src={image4} alt="Ranging Error Graph" /> */}
       </div>
     </div>
   );
